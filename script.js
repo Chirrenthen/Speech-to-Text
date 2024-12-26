cae5ad5b371ee7d7e983109bc9bc6b5f72cbab9f
Chirrenthen <chirrenthen13@gmail.com>
2024-12-26 17:51:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+    const currentIndex = event.resultIndex;
+    const transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(transcript.toLowerCase());
+};
+
+btn.addEventListener('click', () => {
+    content.textContent = "Listening...";
+    recognition.start();
+});
